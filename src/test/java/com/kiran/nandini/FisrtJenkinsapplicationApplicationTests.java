a00a1d633588d03a2c9cc185fbf83f5b3b687c24
kiran <kiranpatil123>
2021-03-20 10:36:57 +0530

    firstcommit

---
+++ src/test/java/com/kiran/nandini/FisrtJenkinsapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.nandini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisrtJenkinsapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
